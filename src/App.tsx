e1afdb7aeeb572f860a7d0aabb4ce3bb00962cde
jahan siddiqui <raj918833@gmail.com>
2024-05-02 01:44:11 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import "./index.css";
+
+function App() {
+  
+
+  return (
+    <>
+      <header>Apple TV +</header>
+      <main>
+        <div>Hero Component</div>
+        <div>Features and Usps</div>
+        <div>column layout</div>
+        <div>carousel posters</div>
+      </main>
+    </>
+  )
+}
+
+export default App
